73e6ba17bbc8c29e889846b78902021dd88d8719
Al-amin Sheikh <azalamins@gmail.com>
2022-04-30 21:26:52 +0600

    restock and deliverd quantity done

--- src/components/InventoryDetails/InventoryDetails.js
+++ src/components/InventoryDetails/InventoryDetails.js
@@ -1,13 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const InventoryDetails = () => {
+  const { manageId } = useParams();
+  const [inventory, setInventory] = useState({});
+  const [updated, setUpdated] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/inventoryDetails?manageId=${manageId}`)
+      .then((res) => res.json())
+      .then((data) => setInventory(data));
+  }, [manageId, updated]);
+
+  const handleDelivered = () => {
+    const quantity = inventory?.quantity - 1;
+    fetch(`http://localhost:5000/updateQuantity?manageId=${manageId}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ quantity }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        toast("Stock updated");
+        setUpdated(data);
+      });
+  };
+
+  const handleRestock = (event) => {
+    event.preventDefault();
+    const userQuantity = event.target.number.value;
+    const quantity = parseInt(inventory?.quantity) + parseInt(userQuantity);
+    fetch(`http://localhost:5000/updateQuantity?manageId=${manageId}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ quantity }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        toast("Added in stock");
+        setUpdated(data);
+        event.target.reset();
+      });
+  };
+
   return (
     <div className="container px-4 mx-auto">
-      <div className="md:flex justify-evenly g-5 mt-10">
-        <div className="">Hello Inventory Details</div>
+      <div className="md:flex justify-evenly md:mt-10 py-10">
+        <div className="md:mr-10 my-10 md:my-0">
+          <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mx-auto text-center">
+            <img className="rounded-t-lg" src={inventory?.img} alt="" />
+            <div className="p-5">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {inventory?.name}
+              </h5>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Price: {inventory?.price}
+              </h5>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Quantity: {inventory?.quantity}
+              </h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                ID:
+                <small> {inventory?._id}</small>
+              </p>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {inventory?.description}
+              </p>
+              <button
+                onClick={handleDelivered}
+                className="w-full bg-indigo-700 hover:bg-indigo-800 py-2 rounded-2xl text-lg text-white  transition-all duration-300 ease-in-out focus:shadow-outline focus:outline-none"
+              >
+                Delivered
+              </button>
+            </div>
+          </div>
+        </div>
         <div>
           <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto ">
-            <form className="space-y-6" action="#">
+            <form className="space-y-6" onSubmit={handleRestock}>
               <h5 className="text-4xl font-medium text-gray-900 dark:text-white text-center">
                 RESTOCK
               </h5>
