0aad7aa185c8d5058cec8c5b41c9a8a0432ab9f8
Al-amin Sheikh <azalamins@gmail.com>
2022-04-30 07:18:49 +0600

    banner heading done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,31 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Hello WOrld</h1>
+      <div
+        style={{ backgroundColor: "#100827" }}
+        className="min-h-screen text-white"
+      >
+        <div className="md:flex justify-between items-center container mx-auto px-4">
+          <div className="">
+            <img
+              className="md:mt-10 pt-5"
+              src="https://i.ibb.co/chPyQpW/Logistics-amico-1-1.png"
+              alt=""
+            />
+          </div>
+          <div className="order-last md:order-first mt-20 md:mt-0 pb-20 md:pb-0 text-center md:text-left">
+            <h3 className="text-4xl md:text-5xl font-bold font-mono">
+              WELCOME TO
+            </h3>
+            <h3 className="text-4xl md:text-5xl font-bold font-mono">
+              DIGITAL
+            </h3>
+            <h3 className="text-4xl md:text-5xl font-bold font-mono">
+              ELECTRONICS
+            </h3>
+          </div>
         </div>
+      </div>
     );
 };
 
