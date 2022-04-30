f1fe8754075f5a6cbf13b4359ce9aee850ae5aea
Al-amin Sheikh <azalamins@gmail.com>
2022-04-30 07:23:54 +0600

    banner heading done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
       <div
-        style={{ backgroundColor: "#100827" }}
-        className="min-h-screen text-white"
+        style={{ backgroundColor: "#100827", minHeight: '90vh' }}
+        className="text-white"
       >
         <div className="md:flex justify-between items-center container mx-auto px-4">
           <div className="">
@@ -14,7 +14,7 @@ const Home = () => {
               alt=""
             />
           </div>
-          <div className="order-last md:order-first mt-20 md:mt-0 pb-20 md:pb-0 text-center md:text-left">
+          <div className="order-last md:order-first mt-20 md:mt-0 pb-14 md:pb-0 text-center md:text-left">
             <h3 className="text-4xl md:text-5xl font-bold font-mono">
               WELCOME TO
             </h3>
